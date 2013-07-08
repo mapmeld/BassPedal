f6da31e5aed80ee490ff06ac182334eedba72cec
Nick Doiron <ndoiron@mapmeld.com>
2013-07-08 13:09:34 -0400

    change from jsdom to cheerio

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 module.exports = function(app, models, mongoose){
 
-  var jsdom = require("jsdom");
+  var request = require('request');
+  var cheerio = require("cheerio");
 
   app.get('/', function(req, res){
     res.render('kickpedal.jade');
@@ -9,20 +10,7 @@ module.exports = function(app, models, mongoose){
   app.get('/kickjson', function(req, res){
 	var findPlaces = function(places, $, pagenum){
 	    return res.json( $('p.location').length );
-		$('p.location').each(function(located){
-			var myPlace = $(this).text().replace('\n','').replace('\n','').replace('\n','');
-			var foundPlace = false;
-			for(var p=0;p<places.length;p++){
-				if(places[p].name == myPlace){
-					places[p].count++;
-					foundPlace = true;
-					break;
-				}
-			}
-			if(!foundPlace){
-				places.push({"name": myPlace, "count": 1});
-			}
-		});
+		
 		if($('.NS_backers__backing_row').length >= 50 && !req.query["page"]){
 			jsdom.env(
 				'http://www.kickstarter.com/projects/' + req.query["project"] + '/backers?page=' + (pagenum+1),
@@ -38,14 +26,26 @@ module.exports = function(app, models, mongoose){
 		}
 	};
 	
-	jsdom.env(
-	  'http://www.kickstarter.com/projects/' + req.query["project"] + '/backers?cursor=' + ( req.query["page"] || "1" ),
-	  [ 'http://code.jquery.com/jquery-1.9.1.min.js' ],
-	  function(err, window){
-		if(err){ return res.json(err); }
-		findPlaces([ ], window.$, 2);
-	  }
-	);
+    var requestOptions = {
+      'uri': 'http://www.kickstarter.com/projects/' + req.query["project"] + '/backers?cursor=' + ( req.query["page"] || "1" )
+    };
+    request(requestOptions, function (err, response, b) {
+      if(err){
+        return res.json( err );
+      }
+      $ = cheerio.load( b );
+      var places = { };
+      $('p.location').each(function(located){
+		var myPlace = $(this).text().replace('\n','').replace('\n','').replace('\n','');
+		if(places[myplace]){
+		  places[myplace]++;
+		}
+		else{
+		  places[myPlace] = 1;
+		}
+	  });
+	  res.json( places );
+    });
   });
   
 };
